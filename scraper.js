328cd87e96d3570d30fd319f594d8bb974c1ceb7
Mohammad Cheraghi <cheraghi2007@gmail.com>
2025-02-11 23:09:48 +0330

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,11 +1,7 @@
 const { chromium } = require("playwright");
 
-(async () => {
-  // مسیر کروم نصب‌شده روی ویندوز را مشخص کن
-  const browser = await chromium.launch({
-    executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe", 
-    headless: true, // برای اجرای بدون UI
-  });
+export default async function handler(req, res) {
+  const browser = await chromium.launch({ headless: true });
 
   const page = await browser.newPage();
   await page.goto("https://www.eghamat24.com/search/Tehran/03-11-24/1", {
@@ -18,6 +14,7 @@ const { chromium } = require("playwright");
     );
   });
 
-  console.log(hotelNames);
   await browser.close();
-})();
+
+  res.status(200).json({ hotels: hotelNames });
+}
